8d5c348d2cd959ee1af09fd552c24fe27aa355a8
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-01-31 18:38:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const { useState, useEffect, useContext, useRef, useMemo, Children, useLayoutEffect, useCallback } = React;
 
-function Carousel({data, itemWidth, itemsPerBlock}) {
+function ClickTouchMoveCarousel({data, itemWidth, itemsPerBlock}) {
 
-  // const itemWidth = 300
-  // const itemsPerBlock = 3
-
-  //Start and changed position of the mouse cursor
-
-  const offset = Math.ceil(itemsPerBlock/2)
+  const offset = Math.ceil(itemsPerBlock/2) //Number of items per each side of the centered one
   const items1 = data.slice(data.length - offset)
   const items0 = data
   const items2 = data.slice(0, offset)
   const mergedItems = items1.concat(items0, items2)
-
-  console.log(mergedItems)
-
   const [items, setItems] = useState(mergedItems)
 
   const [startPosition, setStartPosition] = useState()
   const [changedPosition, setChangedPosition] = useState(-itemWidth)
+  const transform = 'transform 1s' // transition duration when sliding elements
+  const [smooth, setSmooth] = useState(transform)
+  const [status, setStatus] = useState(false) // make the movement start onMouseDown and stop onMouseUp and onMouseLeave
+  const element = useRef() // reference to the container
+  const containerWidth = itemWidth*items.length
+  const overflowWidth = itemWidth*itemsPerBlock
 
   // Function than changes the ID according to changedPosition value
 
@@ -26,23 +24,7 @@ function Carousel({data, itemWidth, itemsPerBlock}) {
     return (changedPosition/itemWidth) + number
   }
 
-  // const [items, setItems] = useState([
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'},
-  //   {title: 'title'}   ])
-
-  const containerWidth = itemWidth*items.length
-
-  // Changing style of active item
-
-    const overflowWidth = itemWidth*itemsPerBlock
+  // Dynamic styles
 
     const overflowStyle = {
       width: `${overflowWidth}px`,
@@ -53,132 +35,148 @@ function Carousel({data, itemWidth, itemsPerBlock}) {
     }
   
     const itemStyleActive = {
-      backgroundColor: 'red',
-      transform: 'scale(1.1)',
+      // boxShadow: '0.5px 0.5px 3px 0.5px black',
+      transform: 'scale(1.15)',
       width: `${itemWidth}px`,
     }
 
-  const [smooth, setSmooth] = useState('transform 0.5s')
-  const [status, setStatus] = useState(false) // make the movement start on mouseDown
-  const element = useRef()
-  const walk = itemWidth // the length of the move
+    const containerStyle = {
+      transform: `translateX(${changedPosition}px)`,
+      transition: smooth,
+      width: `${containerWidth}px`
+    }
 
-  const xStyle = {
-    transform: `translateX(${changedPosition}px)`,
-    transition: smooth,
-    width: `${containerWidth}px`
-  }
-
-    useEffect(() => {
+  // This dynamically changes the styleId to make the centered element stylized
+  
+  useEffect(() => {
     
     const updatedItems = (items.map((x, index) => {return {...x, styleId: setStyleId(index + 1)}}))
     setItems(updatedItems)
 
   }, [changedPosition])
-  
-  function handleMouseDown(e) {
-    setStatus(true)
-    setSmooth(null)
-    setStartPosition((e.nativeEvent.pageX - element.current.offsetLeft) - changedPosition)
-  }
 
-  function handleMouseMove(e) {
-    if (!status) return;
-    e.preventDefault();
-    changedPosition < -(itemWidth*data.length)-itemWidth || changedPosition > 0 ? null
-    : setChangedPosition((e.nativeEvent.pageX - element.current.offsetLeft) - startPosition)
+
+  // Functions for PC
+
+    function prev() {
+      changedPosition >= -itemWidth ? (setChangedPosition(-itemWidth*data.length-itemWidth), setSmooth(null)) : (setChangedPosition(prev => prev + itemWidth), setSmooth(transform))
     }
 
-    function handleTouchStart(e) {
+    function next() {
+      changedPosition <= -itemWidth*data.length ? (setChangedPosition(0), setSmooth(null)) : (setChangedPosition(prev => prev - itemWidth), setSmooth(transform))
+    }
+    
+    function handleMouseDown(e) {
       setStatus(true)
       setSmooth(null)
-      setStartPosition((e.touches[0].pageX - element.current.offsetLeft) - changedPosition)
+      setStartPosition((e.nativeEvent.pageX - element.current.offsetLeft) - changedPosition)
     }
-  
-    function handleTouchMove(e) {
+
+    function handleMouseMove(e) {
       if (!status) return;
+      e.preventDefault();
       changedPosition < -(itemWidth*data.length)-itemWidth || changedPosition > 0 ? null
-      : setChangedPosition((e.touches[0].pageX - element.current.offsetLeft) - startPosition)
+      : setChangedPosition((e.nativeEvent.pageX - element.current.offsetLeft) - startPosition)
       }
+
+    function handleMouseUpAndLeave() {
+      if (!status) return;
+      setChangedPosition(Math.round(changedPosition/itemWidth)*itemWidth)
+      setSmooth(transform)
+      setStatus(false)
+      changedPosition >= -itemWidth ? prev() : null
+      changedPosition <= -itemWidth*data.length ? next() : null
+    }
+
+  // Functions for mobile devices
+
+      function handleTouchStart(e) {
+          setStatus(true)
+          setSmooth(null)
+          setStartPosition((e.touches[0].pageX - element.current.offsetLeft) - changedPosition)
+        }
+      
+      function handleTouchMove(e) {
+        if (!status) return;
+        changedPosition < -(itemWidth*data.length)-itemWidth || changedPosition > 0 ? null
+        : setChangedPosition((e.touches[0].pageX - element.current.offsetLeft) - startPosition)
+        }
 
       function handleTouchEnd() {
         if (!status) return;
         setChangedPosition(Math.round(changedPosition/itemWidth)*itemWidth)
-        setSmooth('transform 0.5s')
+        setSmooth(transform)
         setStatus(false)
         changedPosition >= -itemWidth ? prev() : null
         changedPosition <= -itemWidth*data.length ? next() : null
       }
 
-    function prev() {
-      changedPosition >= -itemWidth ? (setChangedPosition(-itemWidth*data.length-itemWidth), setSmooth(null)) : (setChangedPosition(prev => prev + itemWidth), setSmooth('transform 0.5s'))
-    }
-  
-    function next() {
-      changedPosition <= -itemWidth*data.length ? (setChangedPosition(0), setSmooth(null)) : (setChangedPosition(prev => prev - itemWidth), setSmooth('transform 0.5s'))
-    }
 
-  function handleMouseUpAndLeave() {
-    if (!status) return;
-    setChangedPosition(Math.round(changedPosition/itemWidth)*itemWidth)
-    setSmooth('transform 0.5s')
-    setStatus(false)
-    changedPosition >= -itemWidth ? prev() : null
-    changedPosition <= -itemWidth*data.length ? next() : null
-  }
+  // This make the transition between the last and the first items smoothly infinite
 
   useEffect(() => {
     changedPosition === 0 ? next() : null
     changedPosition === -itemWidth*data.length-itemWidth ? prev() : null
-    return () => {
-      
-    }
   }, [changedPosition])
-  
 
   console.log(changedPosition)
 
   return <>
   
-    <main style={overflowStyle}>
+    <div className='overflow-container' style={overflowStyle}>
 
-    <div ref={element} className='section' style={xStyle}
+    <div ref={element} className='container' style={containerStyle}
     onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} 
     onMouseUp={handleMouseUpAndLeave} onMouseLeave={handleMouseUpAndLeave}
     onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}
     onTouchEnd={handleTouchEnd} onTouchCancel={handleTouchEnd}>
       
-    {items.map((x)=>
-        <div style={x.styleId === Math.round(itemsPerBlock/2) ? itemStyleActive : itemStyleNonActive} className='item'>
-          <div className='inner-content'>
-            <div style={{width: '100px', height: '40px', backgroundImage: `url(${x.img})`}}></div>
-            <p>{x.title}</p>
-            <p>{x.description}</p>
+    {items.map((x, index)=>
+        <div key={index} style={x.styleId === Math.round(itemsPerBlock/2) && !status? itemStyleActive : itemStyleNonActive} className='item'>
+          <div className='item-content'>
+            <div style={{backgroundImage: `url(${x.img})`}}></div>
+            <div style={x.styleId === Math.round(itemsPerBlock/2) && !status ? {height:'23%'} : {}}><p>{x.title}</p><p>{x.description}</p></div>
             </div>
         </div>
         )}
     </div>
 
-  </main> 
+    <div className='prev-btn' onClick={prev}><svg xmlns="http://www.w3.org/2000/svg" fillRule="currentColor" className="bi bi-arrow-left-square" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm11.5 5.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+    </svg></div>
+    
+    <div className='next-btn' onClick={next}> <svg xmlns="http://www.w3.org/2000/svg" fillRule="currentColor" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+    </svg></div>
 
-  <button onClick={prev}>prev</button>
-  <button onClick={next}>next</button>
+    <div className='dots-container'>{items.map((x, index)=><button style={
+      {display: index < offset || index > data.length + Math.floor(itemsPerBlock/2) - (itemsPerBlock+1)%2? 'none' : 'block',
+      backgroundColor: x.styleId === Math.round(itemsPerBlock/2) && !status ? 'grey' : 'inherit',
+      color: x.styleId === Math.round(itemsPerBlock/2) && !status ? 'white' : 'inherit'}}
+      onClick={() => itemsPerBlock%2 != 0 ? setChangedPosition((index-Math.floor(itemsPerBlock/2))*-Math.abs(itemWidth))
+      : setChangedPosition(((index+1)-Math.floor(itemsPerBlock/2))*-Math.abs(itemWidth))}>
+      {index - Math.ceil(itemsPerBlock/2) + 1}</button>)}</div>
+
+  </div> 
   </> 
 }
 
 
 function App() {
     const data = [
-    {title: '1', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '2', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '3', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '4', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '5', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '6', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '7', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},
-    {title: '8', description: 'aaa', img: 'https://www.w3schools.com/howto/img_nature_wide.jpg'},   ]
+    {title: 'John Smith 1', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 2', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 3', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 4', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 5', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 6', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 7', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 8', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+    {title: 'John Smith 9', description: 'Junior Developer', img: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'},
+     ]
 
-    return <Carousel data = {data} itemWidth = {100} itemsPerBlock = {4} />
+    return <ClickTouchMoveCarousel data = {data} itemWidth = {300} 
+    itemsPerBlock = {3} />
   
 }
 
